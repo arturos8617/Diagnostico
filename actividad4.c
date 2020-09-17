2d456b5c8a5acfe0e1235830664b36d299de1de5
Arturo Sanchez <arturo.sanchez7480@alumnos.udg.mx>
2020-09-17 11:03:09 -0500

    Condicion para revisar la calidad del carro

--- actividad4.c
+++ actividad4.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-main(){
-	int y=0;
-	struct car{
+struct car{
 		char tipo_llantas[50];
 		char modelo[50];
 		int no_velocidades;
 		int velo_maxima;
 	}a[2];
+main(){
+	int y=0;
 	while(y<2){
 		printf("Ingresa el tipo de llantas\n ");
-		fgets("%s",a[y].tipo_llantas);
+		scanf("%s",a[y].tipo_llantas);
 		
 		printf("Ingresa el modelo\n ");
 		scanf("%s",a[y].modelo);
@@ -23,5 +22,19 @@ main(){
 		scanf("%d",&a[y].velo_maxima);
 		
 		y++;
+	}
+	condicion();
+}
+condicion (){
+	int x=0;
+	while(x<2){
+		if(a[x].no_velocidades>=5 & a[x].velo_maxima>=150){
+			printf("Tu carro %d es de buen nivel\n",x+1);
+		}
+		else{
+			printf("Tu carro %d es algo viejo\n",x+1);
+		}
+		x++;
+		
 	}
 }
